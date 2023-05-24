28c295e2424cda6f0f963144bc74cf7f77bacd72
Thiana <thianacantunes@gmail.com>
2023-05-24 16:56:17 -0300

    Teste

--- src/controllers/hoteis.controllers.js
+++ src/controllers/hoteis.controllers.js
@@ -5,9 +5,7 @@ export async function getHoteis (req, res) {
     try{
         const hoteis = await db.query(`SELECT * FROM hoteis;`)
         res.status(200).send(hoteis.rows)
-
     } catch (err){
         res.status(500).send(err.message)
     }
-
 }
